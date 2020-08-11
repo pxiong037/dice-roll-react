03ea998e81edbffa9928e6fc37a70b7ff991f973
Prechar <pxiong037@gmail.com>
2020-08-11 04:50:46 +0000

    Updates

---
+++ precache-manifest.631d9c10fdb9749ba43f9f60c1239fd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8562cb83b9b2e02351491b84c9c9f50c",
+    "url": "/dice-roll-react/index.html"
+  },
+  {
+    "revision": "af954c221547f0cdc0ca",
+    "url": "/dice-roll-react/static/css/main.5f37c6db.chunk.css"
+  },
+  {
+    "revision": "ea3af848c235475245ee",
+    "url": "/dice-roll-react/static/js/2.6f50a3d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dice-roll-react/static/js/2.6f50a3d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af954c221547f0cdc0ca",
+    "url": "/dice-roll-react/static/js/main.4bbac6a9.chunk.js"
+  },
+  {
+    "revision": "acc4371cff3cc1407268",
+    "url": "/dice-roll-react/static/js/runtime-main.2ccc0f98.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/dice-roll-react/precache-manifest.631d9c10fdb9749ba43f9f60c1239fd5.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dice-roll-react/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
